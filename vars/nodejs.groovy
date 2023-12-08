61d9b1755271005236c3bc45e54055953056a763
Mukesh.Sahu <msahu201989@gmail.com>
2023-12-08 19:21:48 +0530

    Variables in log file

---
+++ vars/nodejs.groovy
@@ -0,0 +1,47 @@
+def call() {
+    pipeline {
+
+        agent {
+            node 'Workstation'
+        }
+
+        stages {
+
+            stage('Code Quality') {
+                steps {
+                    echo 'Code Quality'
+                }
+            }
+
+            stage('Style Checks') {
+                steps {
+                    echo 'Code Quality'
+                }
+            }
+
+            stage('Unit Tests') {
+                steps {
+                    echo 'Unit tests'
+                }
+            }
+
+            stage('Download Dependencies') {
+                steps {
+                    echo 'Download Dependencies'
+                }
+            }
+
+            stage('Prepare Artifact') {
+                steps {
+                    echo 'Prepare Artifact'
+                }
+            }
+
+            stage('Publish Artifact') {
+                steps {
+                    echo 'Publish Artifact'
+                }
+            }
+        }
+    }
+}
