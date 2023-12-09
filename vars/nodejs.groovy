9770fb0ba57cbc5ac76fc75c9c4761c2a2cc09b0
Mukesh.Sahu <msahu201989@gmail.com>
2023-12-09 11:44:32 +0530

    Variables in log file

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -8,7 +8,7 @@ def call() {
         common.codeQuality()
 
 
-            if ( BRANCH_NAME == "main" || tag ==~ "*") {
+            if ( BRANCH_NAME == "main" || tag ==~ ".*") {
                stage('Style Checks') {
                  echo 'Style Checks'
                }
