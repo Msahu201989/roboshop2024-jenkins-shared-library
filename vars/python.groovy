8f3bc5bc18553523b68d4d327307da2372a97f59
Mukesh.Sahu <msahu201989@gmail.com>
2023-12-11 14:14:34 +0530

    Variables in log file

--- vars/python.groovy
+++ vars/python.groovy
@@ -1,36 +1,11 @@
 def call() {
-    pipeline {
-
-        agent {
-            node 'Workstation'
-        }
-
-        stages {
+    node {
 
 
-            stage('Style Checks') {
-                steps {
-                    echo 'Code Quality'
-                }
-            }
+        common.codeCheckout()
+        common.codeQuality()
+        common.codeChecks()
+        common.artifacts()
 
-            stage('Unit Tests') {
-                steps {
-                    echo 'Unit tests'
-                }
-            }
-
-            stage('Prepare Artifact') {
-                steps {
-                    echo 'Prepare Artifact'
-                }
-            }
-
-            stage('Publish Artifact') {
-                steps {
-                    echo 'Publish Artifact'
-                }
-            }
-        }
     }
 }
