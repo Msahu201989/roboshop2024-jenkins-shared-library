8ea6f3c6b96a9ba8f7144845982e1bb6847465bf
Mukesh.Sahu <msahu201989@gmail.com>
2023-12-11 14:27:10 +0530

    Variables in log file

--- vars/java.groovy
+++ vars/java.groovy
@@ -1,11 +1,17 @@
 def call() {
+    enc.SONAR_EXTRA_OPTS = "-Dsonar.java.binaries=./target"
     node {
 
 
         common.codeCheckout()
+        stage('Compile Code') {
+          sh 'mvn compile'
+        }
         common.codeQuality()
         common.codeChecks()
         common.artifacts()
 
     }
 }
+
+
