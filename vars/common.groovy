cb92cb717727351b9a9810826ff3390f9faf94c9
Mukesh.Sahu <msahu201989@gmail.com>
2023-12-11 14:06:02 +0530

    Variables in log file

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,7 +1,7 @@
 def codeCheckout() {
    stage ('code Checkout') {
-       sh 'find . | sed 1d | xargs rm -rf'
-       git branch: 'main', url: 'https://github.com/msahu201989/${COMPONENT}.git'
+       sh 'find . | sed 1d |xargs rm -rf'
+       git branch: 'main', url: "https://github.com/msahu201989/${COMPONENT}.git"
    }
 }
 
